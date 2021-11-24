cfd79132ab5c5c7fada56764083c0b658f653281
test2doank <36143748+test2doank@users.noreply.github.com>
2021-11-25 01:30:29 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+<title></title>
+
+</header>
+
+<body>
+
+<iframe src="gallery_gdrive.html" width="100%" frameborder="0" style="height: 740px;"></iframe>
+
+
+</main>
+</body>
+</html>
